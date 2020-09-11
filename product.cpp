59eb96c193a69d2ccbf26dc6acef7bb5e832c6ea
Vaid26 <67861541+Vaid26@users.noreply.github.com>
2020-09-12 00:45:55 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+float a,b,n;
+cout<<"Enter two numbers:";
+cin>>a>>b;
+n=a*b;
+cout<<"\nThe product of the numbers is:"<<n;
+return 0;
+
+}
